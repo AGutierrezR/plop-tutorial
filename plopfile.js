cfbe32de64eb7cc5b9e8b677c8adb14c14aeb5a2
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2022-10-05 23:39:35 +0200

    Add basic generator

--- plopfile.js
+++ plopfile.js
@@ -1,5 +1,29 @@
 const config = (plop) => {
-  
+  plop.setGenerator('post', {
+    description: 'Generate file',
+    prompts: [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What do you want to title this post?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is this post about?'
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'content/blog/{{dashCase title}}/input.md',
+        templateFile: '.templates/post.hbs',
+        data: {
+          date: new Date().toISOString()
+        }
+      }
+    ]
+  })
 }
 
 module.exports = config
