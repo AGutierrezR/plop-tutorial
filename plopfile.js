376c25069db41e5c28235b4ab4ca40e6cebac035
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2022-10-06 00:06:00 +0200

    Add custom helpers

--- plopfile.js
+++ plopfile.js
@@ -1,28 +1,30 @@
+const slugify = require('slugify')
+
 const config = (plop) => {
+  plop.setHelper('currentDate', () => new Date().toISOString())
+  plop.setHelper('slugify', (s) => slugify(s, { lower: true, remove: /[']/g }))
+
   plop.setGenerator('post', {
     description: 'Generate file',
     prompts: [
       {
         type: 'input',
         name: 'title',
-        message: 'What do you want to title this post?'
+        message: 'What do you want to title this post?',
       },
       {
         type: 'input',
         name: 'description',
-        message: 'What is this post about?'
+        message: 'What is this post about?',
       },
     ],
     actions: [
       {
         type: 'add',
-        path: 'content/blog/{{dashCase title}}/input.md',
+        path: 'content/blog/{{slugify title}}/input.md',
         templateFile: '.templates/post.hbs',
-        data: {
-          date: new Date().toISOString()
-        }
-      }
-    ]
+      },
+    ],
   })
 }
 
